c23cc785c6c5dae36534c26189b7529c1e5dc5ef
wbradmoore <wbradmoore@gmail.com>
2019-10-06 10:23:16 -0400

    allow changing Normal Mode LED Color by addressing virtual 'LED 0'

--- python_scripts/hs200_led_control.py
+++ python_scripts/hs200_led_control.py
@@ -51,21 +51,28 @@ def set_led_mode(hass, node_id, mode):
 #
 def set_led_color(hass, node_id, led, color):
     VALID_COLORS=['Off', 'Blue', 'Cyan', 'Green', 'Red', 'White', 'Yellow']
-    service_data = {
-            'node_id': int(node_id),
-            'parameter': int(led) + 20,
-            'value': 'Off'
-            }
-
     #
     # Maintain sanity
     #
     if color not in VALID_COLORS:
         logger.warn("'{}' is not a valid color.".format(color))
         return
-    if led < 1 or led > 7:
-        logger.warn("Valid LED values are 1-7. {} is out of range.".format(led))
+    if led < 0 or led > 7:
+        logger.warn("Valid LED values are 0-7. {} is out of range.".format(led))
         return
+
+    if led == 0:
+        parameter = 14
+    else:
+        parameter = led + 20
+
+    service_data = {
+        'node_id': int(node_id),
+        'parameter': parameter,
+        'value': 'Off'
+    }
+
+
     
     service_data['value'] = color
     hass.services.call('zwave', 'set_config_parameter', service_data, False)
@@ -101,42 +108,51 @@ blink_frequency = data.get('blink_frequency', None)
 # Actual work of looping through the group here.
 #
 group = hass.states.get(target_group)
-if group is not None:
-    logger.debug("Found group '{}'.".format(target_group))
-    for name in group.attributes.get('entity_id'):
-        #
-        # Make sure we're actually using the correct HomeSeer switches.
-        #
-        prod = hass.states.get(name).attributes.get('product_name')
-        if prod != 'HS-WD200+ Wall Dimmer' and prod != 'HS-WS200+ Wall Switch':
-            continue
+logger.info(group.attributes)
 
-        node_id = int(hass.states.get(name).attributes.get('node_id'))
-
-        #
-        # Do some work here.
-        #
-        if led_mode is not None:
-            set_led_mode(hass, node_id, led_mode)
-
-        if colors is not None:
-            for key in colors:
-                led = int(key)
-                color = colors[key]
-                logger.info("Setting color '{}' for LED {}.".format(color, led))
-                set_led_color(hass, node_id, led, color)
-        
-        if blink is not None:
-            mask = 0
-            for key in blink:
-                # We need to assemble a bitmask
-                if blink[key]:
-                    mask = mask + (1 << (int(key)-1))
-            set_led_blink(hass, node_id, mask)
-
-        if blink_frequency is not None:
-            set_blink_frequency(hass, node_id, int(blink_frequency) & 255)
-
-else:
+if group is None:
     logger.warn("Group '{}' not found.".format(target_group))
+    exit
+
+if group.attributes.get('node_id') is not None:
+    logger.debug("Found entity '{}'.".format(target_group))
+    entity_names = [target_group]
+else:
+    logger.debug("Found group '{}'.".format(target_group))
+    entity_names = group.attributes.get('entity_id')
+
+for name in entity_names:
+    #
+    # Make sure we're actually using the correct HomeSeer switches.
+    #
+    prod = hass.states.get(name).attributes.get('product_name')
+    if prod != 'HS-WD200+ Wall Dimmer' and prod != 'HS-WS200+ Wall Switch':
+        continue
+
+    node_id = int(hass.states.get(name).attributes.get('node_id'))
+
+    #
+    # Do some work here.
+    #
+    if led_mode is not None:
+        set_led_mode(hass, node_id, led_mode)
+
+    if colors is not None:
+        for key in colors:
+            led = int(key)
+            color = colors[key]
+            logger.info("Setting color '{}' for LED {}.".format(color, led))
+            set_led_color(hass, node_id, led, color)
+    
+    if blink is not None:
+        mask = 0
+        for key in blink:
+            # We need to assemble a bitmask
+            if blink[key]:
+                mask = mask + (1 << (int(key)-1))
+        set_led_blink(hass, node_id, mask)
+
+    if blink_frequency is not None:
+        set_blink_frequency(hass, node_id, int(blink_frequency) & 255)
+
 
